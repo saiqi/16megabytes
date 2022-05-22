5040fd29d071e52f1016352a998bd8fc7134a3eb
Julien Bernard <julien.bernard.iphone@gmail.com>
2022-05-22 08:24:47 +0000

    Implement cFilter and corresponding parser

--- source/vulpes/core/query.d
+++ source/vulpes/core/query.d
@@ -11,7 +11,7 @@ struct QueryComponent
     mixin(Generate);
 }
 
-QueryComponent[] parse(in string sQuery) @safe pure
+private QueryComponent[] parse(in string sQuery) @safe pure
 {
     import std.string : split;
     import std.algorithm : map;
@@ -60,3 +60,63 @@ unittest
     assert(!r[1].values.length);
     assert(r[1].isWildcard == true);
 }
+
+struct CFilter
+{
+    string key;
+    string[][] values;
+}
+
+private CFilter[] parse(T)(T params) // TODO: implement a constraint on T
+{
+    import std.regex : matchFirst;
+    import std.array : Appender, split, array;
+    import std.algorithm : map;
+
+    enum pattern = "^c\\[(?P<key>.+)\\]$";
+
+    Appender!(CFilter[]) app;
+
+    foreach (p; params.byKeyValue)
+    {
+        auto m = matchFirst(p.key, pattern);
+        if(m.empty) continue;
+
+        auto values = p.value
+            .split(",")
+            .map!(a => a.split("+"))
+            .array;
+        app.put(CFilter(m["key"], values));
+    }
+
+    return app.data;
+
+}
+
+unittest
+{
+    import std.algorithm : equal;
+
+    auto p1 = ["c[FOO]" : "BAR"];
+    auto r1 = parse(p1);
+    assert(r1.length == 1);
+    assert(r1[0].values[0][0] == "BAR");
+
+    auto p2 = ["c[FOO]" : "BAR", "other": "none"];
+    assert(parse(p2).length == 1);
+
+    auto p3 = ["c[FOO_0]" : "A,B", "c[FOO_1]": "C,D+E"];
+    auto r3 = parse(p3);
+    assert(r3.length == 2);
+    assert(r3[0].values.equal([["A"], ["B"]]));
+    assert(r3[1].values.equal([["C"], ["D", "E"]]));
+}
+
+struct DataQuery
+{
+    QueryComponent[] components;
+    CFilter[] filters;
+
+    mixin(Generate);
+}
+
