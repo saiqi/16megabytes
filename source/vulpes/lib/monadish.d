3e37bee37d28be5048f5702e2cfa65a03cdd63a3
Julien Bernard <julien.bernard.iphone@gmail.com>
2022-08-25 22:06:48 +0200

    Implement Result type

--- source/vulpes/lib/monadish.d
+++ source/vulpes/lib/monadish.d
@@ -2,6 +2,7 @@ module vulpes.lib.monadish;
 
 import std.typecons : Nullable;
 import std.traits : Unqual, TemplateArgsOf;
+import std.sumtype : SumType, match;
 import std.range;
 
 enum bool isNullable(T) = is(T: Nullable!Arg, Arg);
@@ -76,4 +77,158 @@ unittest
     assert(!convertLookup!(bool, string)(aa, "b").isNull);
     assert(!convertLookup!(string, string)(aa, "c").isNull);
     assert(convertLookup!(uint, string)(aa, "d").isNull);
+}
+
+alias Result(T) = SumType!(Error, T);
+enum bool isResult(Result) = is(Result: SumType!(E, T), T, E : Error);
+template ResultOf(Result)
+{
+    static assert(isResult!Result);
+    alias ResultOf = TemplateArgsOf!Result[1];
+}
+
+bool isError(Result)(auto ref Result r)
+if(isResult!Result)
+{
+    alias T = ResultOf!Result;
+    return r.match!(
+        (inout(Error) e) => true,
+        (inout(T) t) => false
+    );
+}
+
+@safe unittest
+{
+    Result!int v = 1;
+    assert(!v.isError);
+}
+
+unittest
+{
+    Result!int divide(int a, int b) nothrow
+    {
+        Result!int result;
+        if(b == 0) result = new Error("Division by zero!");
+        else result = a / b;
+        return result;
+    }
+
+    auto d24 = divide(2, 4);
+    assert(!d24.isError);
+
+    auto d20 = divide(2, 0);
+    assert(d20.isError);
+}
+
+T get(Result, T)(auto ref Result r, T default_)
+if(isResult!Result && is(T : ResultOf!Result))
+{
+    return r.match!(
+        (inout(Error) e) => default_,
+        (inout(T) t) => t
+    );
+}
+
+unittest
+{
+    Result!int err = new Error("");
+    assert(err.get(0) == 0);
+
+    Result!int ok = 1;
+    assert(ok.get(0) == 1);
+}
+
+auto toRange(Result)(auto ref Result r)
+if(isResult!Result)
+{
+    static struct ResultRange
+    {
+        private:
+        Result result_;
+        bool isEmpty;
+        alias T = ResultOf!Result;
+
+        public:
+        this(Result result)
+        {
+            result_ = result;
+            isEmpty = isError(result_);
+        }
+
+        @property bool empty() inout
+        {
+            return isEmpty;
+        }
+
+        @property T front() inout
+        {
+            assert(!empty);
+            return result_.get(T.init);
+        }
+
+        alias back = front;
+
+        void popFront()
+        {
+            assert(!empty);
+            isEmpty = true;
+        }
+
+        void popBack()
+        {
+            popFront();
+        }
+
+        @property inout(typeof(this)) save() inout
+        {
+            return this;
+        }
+
+        @property size_t length() inout
+        {
+            return !empty;
+        }
+    }
+
+    return ResultRange(r);
+}
+
+unittest
+{
+    import std.algorithm : equal;
+
+    Result!int v = 1;
+    auto rangeV = v.toRange;
+    assert(!rangeV.empty);
+    assert(rangeV.length == 1);
+    assert(rangeV.equal([1]));
+
+    auto copy = rangeV.save();
+    rangeV.popFront();
+    assert(rangeV.empty);
+    assert(!copy.empty);
+
+    Result!int err = new Error("");
+    auto rangeErr = err.toRange;
+    assert(rangeErr.empty);
+    assert(rangeErr.length == 0);
+}
+
+unittest
+{
+    import std.algorithm : joiner, equal;
+    import std.range;
+
+    static struct Record
+    {
+        int id;
+        string name;
+    }
+
+    Result!Record foo = Record(1, "foo");
+    Result!Record bar = Record(2, "bar");
+    Result!Record err = new Error("");
+    assert([foo.toRange, err.toRange, bar.toRange]
+        .joiner
+        .equal([Record(1, "foo"), Record(2, "bar")]));
 }
